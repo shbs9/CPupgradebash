93469e62974e68451259b3e7338c9cf4038bf98f
Shoaib khan khattak <39294639+shbs9@users.noreply.github.com>
2023-01-12 14:06:49 +0500

    update bash

---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+screen
+sh <(curl https://raw.githubusercontent.com/usmannasir/cyberpanel/stable/preUpgrade.sh || wget -O - https://raw.githubusercontent.com/usmannasir/cyberpanel/stable/preUpgrade.sh)
